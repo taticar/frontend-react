791d2f936fb434545c9806f2d9781fdeec6c60f7
taticar <carvalho.t@aluno.ifsp.edu.br>
2023-09-18 18:00:08 -0300

    CardUser

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,6 +2,14 @@ import Header from '../layout/Header'
 import Sidebar from '../layout/Sidebar'
 import Content from '../layout/Content'
 import Footer from '../layout/Footer'
+import CardUser from '../ui/CardUser'
+
+const users = {
+  name: "fdsafdsf",
+  email: "fg@gmail",
+  photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmfF00-q6JffFrYIExZ6aucRIFx3sJFBEtA&usqp=CAU"
+}
+
 
 
 const About = () => {
@@ -12,6 +20,13 @@ const About = () => {
       <div id='main'>
         <Content>
           <h1>Quem Somos</h1>
+          <CardUser user={user}/>
+          <CardUser user={{
+            nome: "Maria",
+            email: "maria@gmail.com",
+            photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQO4LLq9jsK36mE3uNmZLU7SM_Ftmf3OYUsQ&usqp=CAU"
+          }}/>
+          <CardUser/>
         </Content>
         <Sidebar />
       </div>
