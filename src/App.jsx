1abd0d23de9d3c2dd46af43696149709b5966173
taticar <carvalho.t@aluno.ifsp.edu.br>
2023-11-06 14:42:10 -0300

    consertando a cópia do Renan

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./components/pages/Home"
-import QuemSomos from "./components/pages/QuemSomos"
+import About from "./components/pages/About"
 import Page404 from "./components/pages/Page404"
-import Contato from "./components/pages/Contato"
+import Contact from "./components/pages/Contact"
 import {Routes, Route} from "react-router-dom"
 import "./App.css"
 import Initializer from "./components/others/Initializer"
@@ -14,7 +14,7 @@ const App = () => {
       <Route path='/' element={<Home/>} />
 
       <Route path='/quem-somos' element={<About/>} />
-      <Route path='/produtos' element={<Products/>}/>
+
       <Route path='/contato' element={<Contact/>}/>
       <Route path='*' element={<Page404/>}/>
     </Routes>
